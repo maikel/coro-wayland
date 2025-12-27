631c915637580cc3139ecfef5fc992ddc98baae3
Maikel Nadolski <maikel.nadolski@gmail.com>
2025-12-27 09:32:15 +0100

    Basic implementation of immediate tasks in IoContext

--- src/core/IoContext.cpp
+++ src/core/IoContext.cpp
@@ -1,3 +1,95 @@
 // SPDX-License-Identifier: MIT
 
 #include "IoContext.hpp"
+
+#include <sys/poll.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+#include <system_error>
+
+namespace ms
+{
+IoContext::IoContext()
+{
+    mWakeupHandle = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (mWakeupHandle == -1)
+    {
+        throw std::system_error(errno, std::generic_category(), "Failed to create eventfd");
+    }
+}
+
+IoContext::~IoContext()
+{
+    ::close(mWakeupHandle);
+}
+
+void IoContext::enqueue(IoContextTask& task)
+{
+    {
+        std::lock_guard lock(mTasksMutex);
+        mTasks.push_back(&task);
+    }
+    uint64_t value = 1;
+    while (::write(mWakeupHandle, &value, sizeof(value)) == -1) {
+        if (errno != EINTR) {
+            throw std::system_error(errno, std::generic_category(), "Failed to write to eventfd");
+        }
+    }
+}
+
+void IoContext::run() noexcept
+try {
+    std::vector<IoContextTask*> tasksToProcess;
+    while (true)
+    {
+        {
+            std::lock_guard lock(mTasksMutex);
+            if (mStopRequested && mTasks.empty())
+            {
+                break;
+            }
+            tasksToProcess.swap(mTasks);
+        }
+
+        for (IoContextTask* task : tasksToProcess)
+        {
+            task->doCompletion(task);
+        }
+        tasksToProcess.clear();
+
+        ::pollfd fds[] = {
+            {mWakeupHandle, POLLIN, 0},
+        };
+        if (::poll(fds, 1, -1) == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                throw std::system_error(errno, std::generic_category(), "poll() failed");
+            }
+        }
+
+        if (fds[0].revents & POLLIN)
+        {
+            uint64_t value;
+            ::read(mWakeupHandle, &value, sizeof(value));
+        }
+    }
+} catch (...) {
+    // Explicit terminate for static analysis tools that warn about noexcept violations
+    std::terminate();
+}
+
+void IoContext::request_stop()
+{
+    {
+        std::lock_guard lock(mTasksMutex);
+        mStopRequested = true;
+    }
+    uint64_t value = 1;
+    while (::write(mWakeupHandle, &value, sizeof(value)) == -1) {
+        if (errno != EINTR) {
+            throw std::system_error(errno, std::generic_category(), "Failed to write to eventfd");
+        }
+    }
+}
+} // namespace ms
--- src/core/include/IoContext.hpp
+++ src/core/include/IoContext.hpp
@@ -11,21 +11,29 @@
 namespace ms {
 
 struct IoContextTask {
-  enum class Kind { Immediate, Delayed, Poll };
+  enum class Kind { Immediate, Delayed, Poll, StopDelayed, StopPoll };
 
-  void (*doCompletion)() noexcept;
+  void (*doCompletion)(IoContextTask*) noexcept;
   Kind kind;
   std::chrono::steady_clock::time_point scheduledTime;
   std::chrono::steady_clock::duration delay;
   int pollFd;
   short pollEvents;
+  IoContextTask* stopTarget;
 };
 
 class IoContext {
 public:
-  void schedule(IoContextTask& task);
+  IoContext();
+  IoContext(const IoContext&) = delete;
+  IoContext& operator=(const IoContext&) = delete;
+  IoContext(IoContext&&) = delete;
+  IoContext& operator=(IoContext&&) = delete;
+  ~IoContext();
 
-  void run();
+  void enqueue(IoContextTask& task);
+
+  void run() noexcept;
   void request_stop();
 
 private:
@@ -34,5 +42,12 @@ private:
   bool mStopRequested = false;
   int mWakeupHandle;
 };
+
+// class IoScheduler {
+// public:
+//   explicit IoScheduler(IoContext& context) : mContext(context) {}
+
+//   ImmediateAwaitable schedule(IoContextTask& task);
+// };
 
 } // namespace ms
